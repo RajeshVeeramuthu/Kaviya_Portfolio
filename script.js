ecfa6ea54c5dea8ad37d2d47a81876529c957ee7
Rajesh Veeramuthu <140933435+RajeshVeeramuthu@users.noreply.github.com>
2026-09-02 12:16:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Modern Portfolio Loaded");
+
+/* TYPING EFFECT */
+new Typed(".typing", {
+    strings: [
+        "Backoffice Operations Specialist",
+        "Data Processing Associate",
+        "Senior Grade Typist (Distinction)",
+        "SLA & Workflow Specialist",
+        "Administrative Coordinator"
+    ],
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true
+});
+
+/* AOS */
+AOS.init({
+    duration: 1000,
+    once: true
+});
